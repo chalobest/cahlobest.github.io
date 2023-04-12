7659bf93e398a0ae293e31bb913716268ffff286
Arun Ganesh <arun.planemad@gmail.com>
2023-04-13 02:37:47 +0530

    Add stop directions

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiY2xnMG11YjdjMTBseTNzcXJ6bGp4b3BvZSJ9.vUb221BNvz-mtF3rNAEMhw';
 
+let appData = {
+    stopDirections : {},
+    userLocation : {},
+    nearestStops : []
+}
+
 // Set bounds to Mumbai.
 const bounds = [[72.39674, 18.82311], [73.41535, 19.44571]]
 
@@ -39,6 +45,25 @@ map.on('load', () => {
         showBusStopsAtPoint()
     }
 
+    initMap()
+
+    function initMap() {
+        map.addSource('walking-route', {
+            'type': 'geojson',
+            'data': null
+        })
+        map.addLayer({
+            'id': 'walking-route',
+            'type': 'line',
+            'source': 'walking-route',
+            'layout': {},
+            'paint': {
+                'line-color': 'white',
+                'line-opacity': 1,
+                'line-width': 2
+            }
+        });
+    }
 });
 
 // Find nearest stops after the map is moved and zoomed in
@@ -84,9 +109,6 @@ function filterBusRoutes(route_ids) {
     map.setFilter('bus-routes selected label info', route_ids.length ? ['in', 'id', ...route_ids] : ['in', 'id', null])
 }
 
-function filterBusStops(stop_ids) {
-    map.setFilter('bus-stops selected', stop_ids.length ? ['in', 'id', ...stop_ids] : null)
-}
 
 function showBusStopsAtPoint(point) {
 
@@ -95,17 +117,42 @@ function showBusStopsAtPoint(point) {
     }
 
     // Query the 'bus-stop' layer for rendered features
-    const features = map.queryRenderedFeatures({ layers: ['bus-stops'] });
+    let features = map.queryRenderedFeatures({ layers: ['bus-stops'] });
 
-    // Find the nearest bus stop point feature to the current center
-    const nearest = turf.nearestPoint([point.lng, point.lat], turf.featureCollection(features));
+    // Create a list of the nearest bus stops
+    features.forEach( f => f.properties["distance"] = turf.distance([point.lng, point.lat],f.geometry.coordinates) )
+    appData["nearestStops"] = features.sort((a, b) => a.properties.distance - b.properties.distance).slice(0,10)
 
-    // Get the coordinates of the nearest bus stop point feature
-    const nearestCoords = nearest.geometry.coordinates;
+    filterBusStops([appData["nearestStops"][0].properties.id])
 
-    filterBusStops([nearest.properties.id])
+    showWalkingRoute(new mapboxgl.LngLat(appData["nearestStops"][0].geometry.coordinates[0], appData["nearestStops"][0].geometry.coordinates[1]))
 
-    findStopEta(nearest)
+    findStopEta(appData["nearestStops"][0])
+
+}
+
+
+function filterBusStops(stop_ids) {
+    map.setFilter('bus-stops selected', stop_ids.length ? ['in', 'id', ...stop_ids] : null)
+}
+
+function showWalkingRoute(to) {
+
+    const from = map.getCenter()
+
+    // https://docs.mapbox.com/playground/directions/
+    const mapboxDirectionsUrl = `https://api.mapbox.com/directions/v5/mapbox/walking/${from.lng},${from.lat};${to.lng},${to.lat}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=${mapboxgl.accessToken}`
+
+    fetch(mapboxDirectionsUrl).then((response) => response.json())
+        .then((data) => {
+            appData.stopDirections = data
+
+            setTimeout(updateWalkingTime, 500)
+            function updateWalkingTime(){
+                document.getElementById('walking-time').innerHTML = appData.stopDirections.routes.length ? parseInt(appData.stopDirections.routes[0].duration/60) + 1 + ' mins walk': '' 
+            }
+            map.getSource('walking-route').setData(turf.lineString(data.routes[0].geometry.coordinates))
+        })
 }
 
 function findStopEta(stopFeature) {
@@ -148,11 +195,11 @@ function findStopEta(stopFeature) {
                     const val = JSON.parse(data[routeId][tripId])
 
                     // Skip stale ETA if timestamps are more than 60 mins old
-                    if ( new Date().getTime() - val.tS > 60 * 60 * 1000 )
+                    if (new Date().getTime() - val.tS > 60 * 60 * 1000)
                         continue
 
                     // Skip invalid ETA
-                    if ( val.eta == -1 )
+                    if (val.eta == -1)
                         continue
 
                     const etaObj = {
@@ -165,32 +212,33 @@ function findStopEta(stopFeature) {
                     };
 
                     routeObj["etas"].push(etaObj)
-                    
+
 
                 }
 
                 stopTimetable.push(routeObj)
             }
 
-            console.log(stopTimetable.filter(d=>d.etas.length).sort((a, b) => b.trip_count - a.trip_count))
+            console.log(stopTimetable.filter(d => d.etas.length).sort((a, b) => b.trip_count - a.trip_count))
 
-            let etaHtml = `<h2 class="stop uk-heading">${stopFeature.properties.name}</h2><hr>`
+            let etaHtml = `<h2 class="stop uk-margin-remove">${stopFeature.properties.name} <br><small><span id='walking-time'></span></small></h2><br>
+            Towards <b>${stopFeature.properties.towards_stop}</b> <hr>`
+            console.log(appData.stopDirections)
+            const sortedTimetable = stopTimetable.filter(d => d.etas.length).sort((a, b) => b.trip_count - a.trip_count)
 
-            const sortedTimetable = stopTimetable.filter(d=>d.etas.length).sort((a, b) => b.trip_count - a.trip_count)
-
-            sortedTimetable.forEach( route => {
+            sortedTimetable.forEach(route => {
                 etaHtml += `<div class="eta">
                 <a class="route uk-button uk-button-default" target="_blank" href="https://chalo.com/app/live-tracking/route-map/${route.id}"><b>${route.name}</b></a> ${route.last_stop_name}
-                </div>`
-
-                route.etas.sort(function(a, b){return a.eta_mins - b.eta_mins}).forEach( eta => {
-                    etaHtml += `<span uk-tooltip="Location updated ${eta.updated_mins < 1 ? "now" : eta.updated_mins + "mins ago"} ">
-                <b>${Math.floor(eta.eta_mins / 60)}m</b>${eta.updated_mins > 3 ? '<img width=15 src="./assets/yellow-pulsing-dot.gif">' : '<img width=15 src="./assets/green-pulsing-dot.gif">' }
+                </div><div><a href="https://chalo.com/app/live-tracking/time-table/${route.id}">Timetable</a>`
+                
+                route.etas.sort(function (a, b) { return a.eta_mins - b.eta_mins }).forEach(eta => {
+                    etaHtml += `<span class="uk-float-right" uk-tooltip="Vehicle no. ${eta.vehicle_no} updated ${eta.updated_mins < 1 ? "now" : eta.updated_mins + " mins ago"}.">
+                <b>${Math.floor(eta.eta_mins / 60)}m</b>${eta.updated_mins > 3 ? '<img width=15 src="./assets/yellow-pulsing-dot.gif">' : '<img width=15 src="./assets/green-pulsing-dot.gif">'}
                 </span>`
 
                 })
 
-                etaHtml += `</div>`
+                etaHtml += `</div><hr>`
 
             })
 
