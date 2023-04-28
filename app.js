508ca34c6fac6fe62a45117091bde778b718a3e5
Arun Ganesh <arun.planemad@gmail.com>
2023-04-29 02:52:37 +0530

    New style

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiY2xnMG11YjdjMTBseTNzcXJ6bGp4b3BvZSJ9.vUb221BNvz-mtF3rNAEMhw';
 
 let appData = {
-    stopDirections : {},
-    userLocation : {},
-    nearestStops : []
+    stopDirections: {},
+    userLocation: {},
+    nearestStops: []
 }
 
 // Set bounds to Mumbai.
@@ -12,7 +12,7 @@ const bounds = [[72.39674, 18.82311], [73.41535, 19.44571]]
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/planemad/clfuxjmx7002k01nxn4o2cbla', // style URL
+    style: 'mapbox://styles/planemad/clh0ypb5o00e701qt5nw9f67y', // style URL
     center: [72.86, 19.1], // starting position [lng, lat]
     zoom: 10, // starting zoom
     hash: true,
@@ -45,31 +45,35 @@ map.on('load', () => {
         showBusStopsAtPoint()
     }
 
-    initMap()
+    setupMap()
 
-    function initMap() {
+    function setupMap() {
+
         map.addSource('walking-route', {
             'type': 'geojson',
             'data': null
         })
+
         map.addLayer({
             'id': 'walking-route',
             'type': 'line',
             'source': 'walking-route',
             'layout': {},
             'paint': {
-                'line-color': 'white',
+                'line-color': 'black',
                 'line-opacity': 1,
                 'line-width': 2
             }
         });
     }
+
 });
 
 // Find nearest stops after the map is moved and zoomed in
 map.on('moveend', () => {
     if (map.getZoom() > 14) {
         showBusStopsAtPoint()
+        showBusRoutesAtPoint(map.getCenter(), 30)
     }
 })
 
@@ -89,7 +93,7 @@ function showBusRoutesAtPoint(point, bufferPixels) {
     ];
     // Find features intersecting the bounding box.
     const selectedFeatures = map.queryRenderedFeatures(bbox, {
-        layers: ['bus-routes']
+        layers: ['mumbai-bus-routes']
     });
 
     const route_ids = selectedFeatures.map(
@@ -102,11 +106,10 @@ function showBusRoutesAtPoint(point, bufferPixels) {
 function filterBusRoutes(route_ids) {
     // Set a filter matching selected features by FIPS codes
     // to activate the 'counties-highlighted' layer.
-    map.setFilter('bus-routes', route_ids.length ? ['in', 'id', ...route_ids] : null)
-    map.setFilter('bus-routes label', route_ids.length ? ['in', 'id', ...route_ids] : null)
-    map.setFilter('bus-routes selected', route_ids.length ? ['in', 'id', ...route_ids] : null)
-    map.setFilter('bus-routes selected label heading', route_ids.length ? ['in', 'id', ...route_ids] : ['in', 'id', null])
-    map.setFilter('bus-routes selected label info', route_ids.length ? ['in', 'id', ...route_ids] : ['in', 'id', null])
+    map.setFilter('mumbai-bus-routes', route_ids.length ? ['in', 'id', ...route_ids] : null)
+    map.setFilter('mumbai-bus-routes label', route_ids.length ? ['in', 'id', ...route_ids] : null)
+    map.setFilter('mumbai-bus-routes selected', route_ids.length ? ['in', 'id', ...route_ids] : null)
+    map.setFilter('mumbai-bus-routes label', route_ids.length ? ['in', 'id', ...route_ids] : ['in', 'id', null])
 }
 
 
@@ -117,11 +120,11 @@ function showBusStopsAtPoint(point) {
     }
 
     // Query the 'bus-stop' layer for rendered features
-    let features = map.queryRenderedFeatures({ layers: ['bus-stops'] });
+    let features = map.queryRenderedFeatures({ layers: ['mumbai-bus-stops terminal label','mumbai-bus-stops label'] });
 
     // Create a list of the nearest bus stops
-    features.forEach( f => f.properties["distance"] = turf.distance([point.lng, point.lat],f.geometry.coordinates) )
-    appData["nearestStops"] = features.sort((a, b) => a.properties.distance - b.properties.distance).slice(0,10)
+    features.forEach(f => f.properties["distance"] = turf.distance([point.lng, point.lat], f.geometry.coordinates))
+    appData["nearestStops"] = features.sort((a, b) => a.properties.distance - b.properties.distance).slice(0, 10)
 
     filterBusStops([appData["nearestStops"][0].properties.id])
 
@@ -133,7 +136,7 @@ function showBusStopsAtPoint(point) {
 
 
 function filterBusStops(stop_ids) {
-    map.setFilter('bus-stops selected', stop_ids.length ? ['in', 'id', ...stop_ids] : null)
+    map.setFilter('mumbai-bus-stops stop selected', stop_ids.length ? ['in', 'id', ...stop_ids] : null)
 }
 
 function showWalkingRoute(to) {
@@ -148,8 +151,8 @@ function showWalkingRoute(to) {
             appData.stopDirections = data
 
             setTimeout(updateWalkingTime, 500)
-            function updateWalkingTime(){
-                document.getElementById('walking-time').innerHTML = appData.stopDirections.routes.length ? parseInt(appData.stopDirections.routes[0].duration/60) + 1 + ' mins walk': '' 
+            function updateWalkingTime() {
+                document.getElementById('walking-time').innerHTML = appData.stopDirections.routes.length ? parseInt(appData.stopDirections.routes[0].duration / 60) + 1 + ' mins walk' : ''
             }
             map.getSource('walking-route').setData(turf.lineString(data.routes[0].geometry.coordinates))
         })
@@ -166,7 +169,7 @@ function findStopEta(stopFeature) {
             // Find all routes in map view
             // Find features intersecting the bounding box.
             const busRoutes = map.queryRenderedFeatures({
-                layers: ['bus-routes']
+                layers: ['mumbai-bus-routes']
             });
 
             let stopTimetable = []
@@ -231,7 +234,7 @@ function findStopEta(stopFeature) {
                 <a class="route uk-button uk-button-default" target="_blank" href="https://chalo.com/app/live-tracking/route-map/${route.id}"><b>${route.name}</b></a> 
                 <span uk-tooltip="${route.stop_name_list}">${route.last_stop_name}</span>
                 </div><div><a href="https://chalo.com/app/live-tracking/time-table/${route.id}">Timetable</a>`
-                
+
                 route.etas.sort(function (a, b) { return a.eta_mins - b.eta_mins }).forEach(eta => {
                     etaHtml += `<span class="uk-float-right" uk-tooltip="Vehicle no. ${eta.vehicle_no} updated ${eta.updated_mins < 1 ? "now" : eta.updated_mins + " mins ago"}.">
                 <b>${Math.floor(eta.eta_mins / 60)}m</b>${eta.updated_mins > 3 ? '<img width=15 src="./assets/yellow-pulsing-dot.gif">' : '<img width=15 src="./assets/green-pulsing-dot.gif">'}
